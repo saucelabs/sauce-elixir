dc31ba4db7f7c56884a0cc7297cbbe6c4af4f547
Maciek Urbański <maciej.urbanski@saucelabs.com>
2019-06-13 15:30:32 +0200

    Extract common parts

--- lib/callbacks.ex
+++ lib/callbacks.ex
@@ -10,44 +10,42 @@ defmodule Callbacks do
   end
 
   def handle_call({:get_job, job_id}, _from, credentials) do
-    endpoint = Endpoints.Jobs.get(job_id)
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-    {:reply, response, credentials}
+    Endpoints.Jobs.get(job_id)
+      |> request_call(credentials)
   end
 
   def handle_call({:get_user_jobs, username, options}, _from, credentials) do
-    endpoint = Endpoints.Jobs.get_user_jobs(username)
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials, options)
-    {:reply, response, credentials}
+    Endpoints.Jobs.get_user_jobs(username)
+      |> request_call(credentials, options)
   end
 
   def handle_call({:get_user_build_failed_jobs, username, build_id}, _from, credentials) do
-    endpoint = Endpoints.Jobs.get_user_build_failed_jobs(username, build_id)
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-    {:reply, response, credentials}
+    Endpoints.Jobs.get_user_build_failed_jobs(username, build_id)
+      |> request_call(credentials)
   end
 
   def handle_call({:get_build_jobs, build_id}, _from, credentials) do
-    endpoint = Endpoints.Jobs.get_build_jobs(build_id)
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-    {:reply, response, credentials}
+    Endpoints.Jobs.get_build_jobs(build_id)
+      |> request_call(credentials)
   end
 
   def handle_call({:get_builds}, _from, credentials) do
-    endpoint = Endpoints.Builds.get()
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-    {:reply, response, credentials}
+    Endpoints.Builds.get()
+      |> request_call(credentials)
   end
 
   def handle_call({:get_metrics, job_id}, _from, credentials) do
-    endpoint = Endpoints.Metrics.get(job_id)
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-    {:reply, response, credentials}
+    Endpoints.Metrics.get(job_id)
+      |> request_call(credentials)
   end
 
   def handle_call({:get_metrics_baseline_history, job_id}, _from, credentials) do
-    endpoint = Endpoints.Metrics.get_baseline_history(job_id)
-    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
+    Endpoints.Metrics.get_baseline_history(job_id)
+      |> request_call(credentials)
+  end
+
+  defp request_call(endpoint, credentials, options \\ []) do
+    response = Requester.call(endpoint[:method], endpoint[:url], credentials, options)
     {:reply, response, credentials}
   end
 end
