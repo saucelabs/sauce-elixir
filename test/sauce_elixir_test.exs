58bb4e211f4de6bb946dd3fb25f495ac7e2a50aa
Maciek Urbański <maciej.urbanski@saucelabs.com>
2019-06-13 14:22:06 +0200

    Add resource for getting jobs by build id

--- test/sauce_elixir_test.exs
+++ test/sauce_elixir_test.exs
@@ -22,4 +22,20 @@ defmodule SauceElixirTest do
     )
     assert {:ok, %{"job_details" => "details"}} = Sauce.Jobs.get(server_pid, "12345")
   end
+
+  test "get jobs by build id responds with Http 200", %{bypass: bypass, server_pid: server_pid} do
+    Bypass.expect(
+      bypass,
+      fn conn ->
+        assert "GET" == conn.method
+        assert "/rest/v1/builds/12345/jobs" == conn.request_path
+        Plug.Conn.resp(
+          conn,
+          200,
+          "{\"jobs\": []}"
+        )
+      end
+    )
+    assert {:ok, %{"jobs" => []}} = Sauce.Jobs.get_build_jobs(server_pid, "12345")
+  end
 end
