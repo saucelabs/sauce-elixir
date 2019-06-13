eeb635718e41e4cc65d94af1488c81e383f0c4b9
Pawel Dudzinski <pawel.dudzinski@saucelabs.com>
2019-06-13 10:50:47 +0200

    Implement starting gen server

---
+++ lib/Sauce.ex
@@ -0,0 +1,25 @@
+defmodule Sauce do
+  use GenServer
+
+  # API
+
+  def start(username, access_key, base_url \\ "https://saucelabs.com") do
+    IO.puts("Start sauce client: #{username}")
+    config = %{
+        :username => username,
+        :access_key => access_key,
+        :base_url => base_url
+    }
+    GenServer.start_link(__MODULE__, [config])
+  end
+
+  # CALLBACKS
+
+  def init(state) do
+    credentials = Enum.at(state, 0)
+    IO.puts("Event loop started for: #{Map.get(credentials, :username)}")
+    {:ok, credentials}
+  end
+
+
+end
