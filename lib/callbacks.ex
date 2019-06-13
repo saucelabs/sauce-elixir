85f1e18dd913cd089bb5ee8a2498babf2dbf04df
Pawel Dudzinski <pawel.dudzinski@saucelabs.com>
2019-06-13 13:12:29 +0200

    Format Callbacks

--- lib/callbacks.ex
+++ lib/callbacks.ex
@@ -1,21 +1,21 @@
 defmodule Callbacks do
-    def init([username, access_key, base_url]) do
-        credentials = %{
-            :username => username,
-            :access_key => access_key,
-            :base_url => base_url
-        }
-        {:ok, credentials}
-    end
+  def init([username, access_key, base_url]) do
+    credentials = %{
+    :username => username,
+    :access_key => access_key,
+    :base_url => base_url
+    }
+    {:ok, credentials}
+  end
 
-    def handle_call({:get_job, job_id}, _from, credentials) do
-        endpoint = Endpoints.Jobs.get(job_id)
-        response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-        {:reply, response, credentials}
-    end
-    def handle_call({:get_user_jobs, username}, _from, credentials) do
-        endpoint = Endpoints.Jobs.get_user_jobs(username)
-        response = Requester.call(endpoint[:method], endpoint[:url], credentials)
-        {:reply, response, credentials}
-    end
+  def handle_call({:get_job, job_id}, _from, credentials) do
+    endpoint = Endpoints.Jobs.get(job_id)
+    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
+    {:reply, response, credentials}
+  end
+  def handle_call({:get_user_jobs, username}, _from, credentials) do
+    endpoint = Endpoints.Jobs.get_user_jobs(username)
+    response = Requester.call(endpoint[:method], endpoint[:url], credentials)
+    {:reply, response, credentials}
+  end
 end
